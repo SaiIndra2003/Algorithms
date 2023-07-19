4697efbace173f47a58e46fa3c1de36c98e6297e
SaiIndra2003 <suryasaiindra@gmail.com>
2023-07-19 18:14:51 +0530

    python and java implementation of quick Sort

--- 5. Quick Sort/QuickSort.java
+++ 5. Quick Sort/QuickSort.java
@@ -1,3 +1,55 @@
 public class QuickSort {
-    
+
+    static void printArray(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    static int Partition(int[] arr, int low, int high){
+        int pivot = arr[low];
+        int left = low;
+        int right = high;
+        while(left<right){
+            while(left<high && arr[left]<=pivot){
+                left++;
+            }
+            while(right>left && arr[right]>pivot){
+                right--;
+            }
+            System.out.println(right + " " + left);
+            if(left<right){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }      
+        // System.out.println(arr[low]+ " " + arr[right]);
+        int temp = arr[low];
+        arr[low] = arr[right];
+        arr[right] = temp;      
+        return right;
+    }
+
+    static void quickSort(int[] arr,int low, int high){
+        if(low<high){
+            int p = Partition(arr, low, high);
+            quickSort(arr, low, p-1);
+            quickSort(arr, p+1, high);
+        }
+    }
+
+    public static void main(String args[]){
+        int[] arr = new int[] {9,8,7,6,5,4,3,2,1};
+        int low = 0;
+        int high = arr.length-1;
+        System.out.println("Array before sorting... :");
+        printArray(arr);
+        quickSort(arr, low,high);
+        System.out.println("Arraay after sorting... :");
+        printArray(arr);
+
+    }
 }
--- 5. Quick Sort/QuickSort.py
+++ 5. Quick Sort/QuickSort.py
@@ -1,11 +1,26 @@
-def partition(arr,low,high):
+def Partition(arr,low,high):
     pivot = arr[low]
-    left = low
-    right = high
-    while(left<right):
-        while(arr[left]<=pivot):
-            left+=1
-        while(arr[right]>pivot):
-            right-=1
-        if(left<right):
-            temp = 
+    start = low
+    end = high
+    while(start<end):
+        while(start < high and arr[start]<=pivot):
+            start+=1
+        while(end > low and arr[end]>pivot):
+            end-=1
+        if(start<end):
+            arr[start],arr[end] = arr[end],arr[start]
+    arr[low],arr[end] = arr[end], arr[low]
+    return end
+
+def QuickSort(arr,low,high):
+    if(low<high):
+        p = Partition(arr,low,high)
+        QuickSort(arr,low,p-1)
+        QuickSort(arr,p+1,high)
+
+if __name__=="__main__":
+    arr = [9,8,7,6,5,4,3,2,1]
+    print("Array before sorting... :",arr)
+    QuickSort(arr,0,len(arr)-1)
+    print("Array after sorting...",arr)
+    
